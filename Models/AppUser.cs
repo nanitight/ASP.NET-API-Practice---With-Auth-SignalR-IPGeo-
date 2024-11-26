e85a66cb3e08a034486d63266ac88f27b288cb2e
Godiragetse Naane <gnaane@sts3d.co.za>
2024-11-26 13:53:13 +0200

    PK issues solved

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -6,7 +6,7 @@ namespace WebApplication1.Models
     public class AppUser  : IdentityUser
     {
         //[Key]
-        public string Id { get; set; }
+        //public string Id { get; set; }
         public int? Pace{ get; set; }
         public int? Milage { get; set; }
         [ForeignKey("Address")]
